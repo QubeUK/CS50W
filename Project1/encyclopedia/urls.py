6d7776e3a566bc381c60a48b14f0acfb03f69884
QubeUK <lee@openforge.net>
2023-12-14 13:15:29 +0000

    django work

--- Project1/encyclopedia/urls.py
+++ Project1/encyclopedia/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("wiki/create/", views.new_page, name="create"),
     path("wiki/rand/", views.rand, name="rand"),
     path("wiki/display/", views.display, name="display"),
-    #path("<str:entry>", views.rand, name="entry")
+    #path("<str:entry>/", views.rand, name="entry")
 
 ]
--- Project1/wiki/urls.py
+++ Project1/wiki/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
     path('', include("encyclopedia.urls"))
 
 ]
