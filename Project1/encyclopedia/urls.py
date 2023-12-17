64c61031a812f3d1b05cabc940feaf9fd4bdbfca
Lee Williams <QubeUK@users.noreply.github.com>
2023-12-17 19:00:54 +0000

    small progress

--- Project1/encyclopedia/urls.py
+++ Project1/encyclopedia/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/create/", views.new_page, name="create"),
     path("wiki/rand/", views.rand, name="rand"),
-    path("wiki/display/", views.display, name="display"),
+    #path("wiki/display/", views.display, name="display"),
     #path("<str:entry>/", views.rand, name="entry")
+    path("<str:entry>/", views.display, name="display"),
+    path("<int:question_id>/", views.display, name="display"),
+    
 
 ]
--- Project1/encyclopedia/views.py
+++ Project1/encyclopedia/views.py
@@ -1,25 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django import forms
 
 from . import util
 
+class NewWikiForm(forms.Form):
+    article = forms.CharField(label="New Article")
+
 
 def index(request):
+    if "articles" not in request.session:
+        request.session["articles"] = []
+    
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def new_page(request):
+    if request.method == "POST":
+        form = NewWikiForm(request.POST)
+        if form.is_valid():
+            article = form.cleaned_data["article"]            
+            return HttpResponseRedirect(reverse("wiki:index"))
+        else:
+            return render(request, "wiki/create.html", {
+                "form": form
+            })
+
     return render(request, "encyclopedia/create.html", {
-        # "title, contents":util.save_entry()
+        "form": NewWikiForm()
     })
 
 def rand(request):
     
     return render(request, "encyclopedia/rand.html", {
-        # "title":util.get_entry(entry)
+        
     })
 
-def display(request):
-    return render(request, "encyclopedia/rand.html", {
-        #"title":util.get_entry()
-    })
+#def display(request, entry):
+#    return HttpResponse("You're looking at page %s." % entry)
+
+def display(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
