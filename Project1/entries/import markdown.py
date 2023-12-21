54942b5995512e1017391c5950ed2482951e333f
Lee Williams <QubeUK@users.noreply.github.com>
2023-12-21 19:16:45 +0000

    File tidy

--- Project1/entries/import markdown.py
+++
@@ -1,9 +0,0 @@
-import markdown
-
-with open("Git.md", "r") as file:
-    text = file.read()
-    html = markdown.markdown(text)
-    
-    title = html.split()[0].replace("<h1>","").replace("</h1>","")
-    
-print(title)
--- Project1/wiki/settings.py
+++ Project1/wiki/settings.py
@@ -30,8 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+INSTALLED_APPS = [    
     'encyclopedia',
     'django.contrib.admin',
     'django.contrib.auth',
